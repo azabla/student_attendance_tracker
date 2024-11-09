3b8c4d4cf056446eaf5eefd601717a2f782dc315
AzawuntuBlatena <azabla1227@gmail.com>
2024-11-10 00:24:38 +0300

    dark theme

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,12 @@
-import React from 'react'
+"use client"
+import { useTheme } from 'next-themes'
+import React, { useEffect } from 'react'
 
 function dashboard() {
+    const { setTheme } = useTheme()
+    useEffect(() => {
+    setTheme('system')
+    }, [])
   return (
     <div>dashboard</div>
   )
